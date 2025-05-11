ac41a3754574a4dbca3b54dd262179e9b5fcc94e
Napol  Thanarangkaun <lopanapol@gmail.com>
2025-05-11 07:57:33 +0700

    Add assembly utility functions: mcopy, scomp, and slen; implement write_message for output

---
+++ source/utils/asm/slen.s
@@ -0,0 +1,14 @@
+.section __TEXT,__text
+.global _slen
+_slen:
+    movq %rdi, %rax        # Copy string pointer to RAX
+    xorq %rcx, %rcx        # Clear RCX (counter)
+strlen_loop:
+    cmpb $0, (%rax)        # Check if current byte is null
+    je strlen_done         # If null, jump to done
+    incq %rax              # Move to next byte
+    incq %rcx              # Increment counter
+    jmp strlen_loop        # Repeat loop
+strlen_done:
+    movq %rcx, %rax        # Return counter as length
+    ret
